99ea7911f074a001d2e589c233479685e765dbfd
Plexible <cav.alperen@gmail.com>
2024-04-16 22:43:48 +0300

    Main part added.

---
+++ src/main.py
@@ -0,0 +1,66 @@
+from key.create_key import generate_key, embedding_key
+from key.create_secret_key import create_binary_secret_key_list, convert_binary_secret_key_list_byte
+from embedding.embedding import embedding
+from extracting.extracting import extracting
+from deep_learning.deep_learning_model import show_n_generate
+import cv2
+
+from test.test_images import calculate_histogram, calculate_mae, calculate_mse, calculate_psnr, calculate_ssim, perform_statistical_test
+
+#String
+secret_key = generate_key()
+print(f"Secret Key: {secret_key}")
+combined_binary = create_binary_secret_key_list(secret_key)
+
+#Image
+image_path = "assets/embedding_images/image2.webp"
+output_path = "assets/embedded_images/embedded_img.png"
+
+#Key
+key = show_n_generate(image_path, greedy = True).encode("utf-8")
+generated_embedding_key = embedding_key(key)
+
+#Embedding Part
+embedded_image = embedding(image_path, combined_binary, generated_embedding_key)   
+
+#Extractin Part
+cv2.imwrite(output_path, embedded_image)
+
+#Test
+# Load images
+original_image = cv2.imread(image_path, 0)
+distorted_image = cv2.imread(output_path, 0)
+# Calculate histograms
+original_histogram = calculate_histogram(original_image)
+distorted_histogram = calculate_histogram(distorted_image)
+# Statistical test
+t_statistic, p_value = perform_statistical_test(original_histogram, distorted_histogram)
+print("T statistic:", t_statistic)
+print("P value:", p_value)
+# İki resmi yükle
+original_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+distorted_image = cv2.imread(output_path, cv2.IMREAD_COLOR)
+# PSNR'yi hesapla
+psnr_value = calculate_psnr(original_image, distorted_image)
+print("PSNR Value:", psnr_value)
+# MSE'yi hesapla
+mse_value = calculate_mse(original_image, distorted_image)
+print("MSE Value:", mse_value)
+# MAE'yi hesapla
+mae_value = calculate_mae(original_image, distorted_image)
+print("MAE Value:", mae_value)
+# SSI'yi hesapla
+ssim_score = calculate_ssim(original_image, distorted_image)
+print("SSI Score:", ssim_score)
+
+
+#Deep Learning Model
+key = show_n_generate(output_path, greedy = True).encode("utf-8")    
+generated_embedding_key = embedding_key(key)
+
+extracted_array = extracting(output_path, generated_embedding_key)
+print(extracted_array)
+
+extracted_secret_key = convert_binary_secret_key_list_byte(extracted_array)
+
+print(extracted_secret_key)
